0d7f9879e9e0d51ac20a2ed1a30762008c457e9c
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-04 16:36:36 +0300

    Add project structure, clean code ,deployed latest build

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## My App is deployed
+
+Here --> check http://myrobofriendsapp.surge.sh/  [4 May 2019]
+
+## Made by Nickolas Benakis
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList.js';
-import SearchBox from './searchBox.jsx';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/searchBox.jsx';
 import './App.css';
-import Scroll from './Scroll.jsx';
+import Scroll from '../components/Scroll.jsx';
 
 
 export default class App extends Component {
@@ -26,11 +26,11 @@ export default class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-        if (this.state.robots.length === 0) {
-            return <h1 style={{ textAlign: "center" }}>Loading</h1>
-        } else {
-            return (
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
+        return (!robots.length) ?
+            <h1 style={{ textAlign: "center" }}>Loading</h1> :
+            (
                 <div className="App">
                     <h1 className="f2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -39,7 +39,8 @@ export default class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
     }
 }
+
+
 
