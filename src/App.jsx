5a1f861ee23cd8eb2641bf2fc11abe6c3e32b245
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-04 15:59:36 +0300

    Add service for robots

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList.js';
-import { robots } from './robots.js';
 import SearchBox from './searchBox.jsx';
 import './App.css';
 
-// const state = {
-//     robots: robots,
-//     searchField: ''
-// }
+
 
 export default class App extends Component {
 
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
 
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -26,14 +27,18 @@ export default class App extends Component {
 
     render() {
         const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-        return (
-            <div className="App">
-                <h1 className="f2">RoboFriends</h1>
-                <hr />
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 style={{ textAlign: "center" }}>Loading</h1>
+        } else {
+            return (
+                <div className="App">
+                    <h1 className="f2">RoboFriends</h1>
+                    <hr />
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
