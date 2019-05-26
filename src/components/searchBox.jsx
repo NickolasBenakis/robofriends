bf86aa86b2c66064f36f5aa7a2bf5e4aea60725f
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-26 14:00:36 +0300

    PWA impl

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -6,6 +6,7 @@ export default function SearchBox({ searchField, searchChange }) {
     return (
         <div className="pa2">
             <input className="pa3 ba b--green bg-lightest-blue"
+                aria-label="Search"
                 type="search" placeholder="search robots" onChange={searchChange} />
         </div>
     );
