6948fc01987b7bc6d70bd04fe02104118b616328
NickolasBenakis <nickolasbenele@gmail.com>
2019-06-05 22:33:55 +0300

    Add more reducers tests

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -13,7 +13,7 @@ describe('searchRobots', () => {
         searchField: ''
     }
 
-    it('should return the inital state', () => {
+    it('should return the initial state', () => {
         expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: '' })
     });
 
@@ -23,6 +23,58 @@ describe('searchRobots', () => {
             payload: 'abc'
         })).toEqual({
             searchField: 'abc'
+        })
+    });
+});
+
+describe('requestRobots', () => {
+    const initialStateRobots = {
+        isPending: false,
+        robots: [],
+        error: ''
+    }
+
+    it('should return the initial state', () => {
+        expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots)
+    });
+
+    it('should handle REQUEST_ROBOTS_PENDING action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_PENDING
+        })).toEqual({
+            robots: [],
+            isPending: true,
+            error: ''
+        })
+    });
+
+    it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_RESOLVED,
+            payload: [{
+                id: '123',
+                name: 'test',
+                email: 'test@gmail.com'
+            }]
+        })).toEqual({
+            robots: [{
+                id: '123',
+                name: 'test',
+                email: 'test@gmail.com'
+            }],
+            isPending: false,
+            error: ''
+        })
+    });
+
+    it('should handle REQUEST_ROBOTS_REJECT action', () => {
+        expect(reducers.requestRobots(initialStateRobots, {
+            type: REQUEST_ROBOTS_REJECT,
+            payload: ""
+        })).toEqual({
+            robots: [],
+            isPending: false,
+            error: ""
         })
     });
 });
